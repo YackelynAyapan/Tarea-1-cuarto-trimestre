836488ec25eb185cf848f770a8e5a10d4da0bafd
YackelynAyapan <yackelynestefany@yahoo.com>
2016-11-18 18:37:44 -0600

    Tarea 2

---
+++ tarea2/tarea2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tarea2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cadena1 = string.Empty;
+            string cadena2 = string.Empty;
+            string resultado = string.Empty;
+
+            cadena1 = "Amin";
+            Console.WriteLine("Ingrese una cadena");
+            cadena2 = Console.ReadLine();
+
+
+            resultado = (cadena1 == cadena2) ? "las cadenas coinciden":"Las cadenas no coinciden" ;
+            Console.WriteLine(resultado);
+            Console.ReadLine();
+
+        //    if (cadena1 == cadena2)
+        //    {
+        //        Console.WriteLine("las cadenas coninciden");
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("las cadenas no coinciden");
+
+        //    }
+        }
+}
+}
